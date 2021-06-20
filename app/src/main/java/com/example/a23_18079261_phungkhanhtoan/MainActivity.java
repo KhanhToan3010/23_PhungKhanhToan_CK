611c305803396b24b0b0b8890190f9058fae20ab
KhanhToan3010 <khanhtoan18079261@gmail.com>
2021-06-20 17:26:38 +0700

    Room chưa hoàn thiện

--- app/src/main/java/com/example/a23_18079261_phungkhanhtoan/MainActivity.java
+++ app/src/main/java/com/example/a23_18079261_phungkhanhtoan/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.a23_18079261_phungkhanhtoan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    Button   btncf, btnsn;
+    EditText plsearch;
+
+    RecyclerView rcv;
+    CustomAdapter adt;
+    List<ItemCF> itemscf;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btncf = findViewById(R.id.btncf);
+        btnsn = findViewById(R.id.btnsn);
+        plsearch = findViewById(R.id.plsearch);
+        rcv = findViewById(R.id.rcv);
+        adt = new CustomAdapter(itemscf,this);
+
+        rcv.setHasFixedSize(true);
+        rcv.setAdapter(adt);
+        rcv.setLayoutManager(new LinearLayoutManager(MainActivity.this, LinearLayoutManager.VERTICAL,false));
+
+        btncf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnsn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
